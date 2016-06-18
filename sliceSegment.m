70716f00d87c6bc1fa05e67620b9617cb1d7e3eb
aalexx-S <alex530519@gmail.com>
2016-06-18 17:16:35 +0800

    add slice segment

---
+++ sliceSegment.m
@@ -0,0 +1,14 @@
+function segment = sliceSegment (au)
+    cBeat = au.cBeat;
+    fs = au.fs;
+    segment = cell(1, length(cBeat) / 4);
+    lbeat = 1;
+    for i = 1:length(cBeat) / 4
+        en = int32(cBeat(i*4) * fs);
+        if (en > length(au.signal))
+            en = length(au.signal);
+        end
+        segment{i} = num2cell( au.signal(lbeat : en) );
+        lbeat = en;
+    end
+end
